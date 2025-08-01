c96d3e86165d024e5c2b9b9f27afbfac02eb2b73
MinoYasuhiro <CA01254021@kawahara.ac.jp>
2025-08-01 13:47:20 +0900

    <Player>弾の発射とフラッシュのアニメーション追加。

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //プレイヤーのアニメーション。
     public Sprite spriteNomal;  //プレイヤーの通常スプライト
     public Sprite spriteDown;   //プレイヤーの下降スプライト
     public Sprite spriteUp;     //プレイヤーの上昇スプライト
 
+    //弾の発射。
+    public GameObject shotPrefab;  //弾のプレハブ
+    public int maxShotCount = 20;  //事前に球を作成する数。
+    public float shotSpeed = 30;   //弾が1秒間に進む距離。
+    List<GameObject> shotList;     //弾キャッシュ（事前に作成する弾。）
+    int nextShotIndex = 0;         //次に発射する弾キャッシュの位置。
+
+    public GameObject shotFlash;   //弾のフラッシュエフェクト
+    const int FLASH_VIEW_FRAME = 4; //発射効果を継続して表示するフレーム数。
+    public int shotPerSec = 8;      //1秒間に発射する弾数。
+    int shotFrame;                 //前の球を発射してからの経過フレーム数。
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //弾の発射。
+        //カメラの後ろ位置。
+        Vector3 hidePos = Camera.main.transform.position - Camera.main.transform.forward;
+
+        //弾を事前に作成し、カメラの後ろに配置。
+        shotList = new List<GameObject>();
+        for(int i = 0; i < maxShotCount; i++)
+        {
+            //プレハブから球を生成。
+            shotList.Add(Instantiate(shotPrefab));
+            shotList[i].transform.position = hidePos;
+        }
+
     }
 
     // Update is called once per frame
@@ -61,6 +86,41 @@ public class Player : MonoBehaviour
             }
 
         }
-        
+
+        //一秒間に発車する段数に応じて次の球を発射するまでのフレーム数。
+        int shotInterval = 50 / shotPerSec;
+
+        //弾発射。
+        shotFrame++;
+        if(shotFrame >=shotInterval)
+        {
+            //次の球待機を開始するため経過時間を初期化。
+            shotFrame = 0;
+
+            //弾をプレイヤー位置に移動。
+            shotList[nextShotIndex].transform.position = transform.position;
+            //次の弾の位置
+            nextShotIndex++;
+            if (nextShotIndex >= maxShotCount)
+            {
+                nextShotIndex = 0;
+            }
+
+            //弾のフラッシュエフェクトを生成。
+            shotFlash.SetActive(true);
+        }
+        else if(shotFrame >=FLASH_VIEW_FRAME)
+        {
+            //発射効果の継続期間が過ぎたら発射効果を非表示。
+            shotFlash.SetActive(false);
+        }
+
+       
+        //弾移動。
+        for(int i = 0; i < maxShotCount; i++)
+        {
+            shotList[i].transform.position += transform.right * shotSpeed * Time.fixedDeltaTime;
+        }
+
     }
 }
