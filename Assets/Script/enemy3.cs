acbc691bc5edf3f6153de22c1e4dbbf96423ce71
SennbaTomohiro <sennbatomohiro@gmail.com>
2025-08-01 11:43:37 +0900

    Sennba/create enemy (#6)
    * <Script>テスト
    
    * <Script>テスト
    
    * <Script>テスト
    
    * <Enemy>エネミーの移動
    
    * <Enemy>エネミーのアニメーション追加
    
    * <Enemy>エネミーの移動とアニメーションの修正

---
+++ Assets/Script/enemy3.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy3 : MonoBehaviour
+{
+    public float speed = 10;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //現在の座標を取得
+        Vector3 pos = transform.position;
+
+        //進行方向に向かって直進
+        pos += -transform.right * speed * Time.fixedDeltaTime;
+
+        //新しい座標に登録
+        transform.position = pos;
+    }
+}
