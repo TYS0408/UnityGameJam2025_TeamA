f530114d59d78c4fe5ae13866519902aaee50c57
unknown <sennbatomohiro@gmail.com>
2025-08-05 14:41:13 +0900

    Revert "<Title>タイトル画面および動作の修正"
    This reverts commit 9ad4bb136520776b0923ceae11317b19c376b0e0.

--- Assets/Script/TitleController.cs
+++ Assets/Script/TitleController.cs
@@ -20,6 +20,6 @@ public class TitleController : MonoBehaviour
 
     public void OnClickStartButton()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("GameScene");
     }
 }
